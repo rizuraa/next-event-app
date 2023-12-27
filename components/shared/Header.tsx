708c7a066177d0ebaa615e41d9b27c77a9c8265e
Muhammad Fariz Maulana <19104035@ittelkom-pwt.ac.id>
2023-12-27 22:40:40 +0700

    SignIn clerk component after login

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,4 +1,4 @@
-import { SignedOut } from "@clerk/nextjs"
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs"
 import Image from "next/image"
 import Link from "next/link"
 import { Button } from "../ui/button"
@@ -14,6 +14,9 @@ const Header = () => {
                     />
                 </Link>
                 <div className="flex w-32 justify-end gap-3">
+                    <SignedIn>
+                        <UserButton afterSignOutUrl="/" />
+                    </SignedIn>
                     <SignedOut>
                         <Button asChild className="rounded-full" size='lg'>
                             <Link href='/sign-in'>
