d9ae254672c7eb351a6fd000267cbb1da19b1431
Muhammad Fariz Maulana <19104035@ittelkom-pwt.ac.id>
2023-12-27 17:57:56 +0700

    initialize shadcn ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
-import '@/app/ui/globals.css';
-import { inter } from './ui/font';
+import './globals.css'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
