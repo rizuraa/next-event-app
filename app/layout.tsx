2bb6697dc5cfeefacaaab12d5fb506917ec103ab
Muhammad Fariz Maulana <19104035@ittelkom-pwt.ac.id>
2023-12-27 14:09:19 +0700

    initial fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 import '@/app/ui/globals.css';
-
-const inter = Inter({ subsets: ['latin'] })
+import { inter } from './ui/font';
 
 export const metadata: Metadata = {
   title: 'Create Next App',
