cb3d7988da3c3a35bc34e471952220b8ee6af6ec
Muhammad Fariz Maulana <19104035@ittelkom-pwt.ac.id>
2023-12-31 01:48:10 +0700

    complete utils n add query-string

---
+++ lib/actions/user.action.ts
@@ -0,0 +1,12 @@
+'use server'
+
+import { CreateUserParams } from "@/types"
+import { handleError } from "../utils"
+
+export const createUser = async (user: CreateUserParams) => {
+    try {
+
+    } catch (error) {
+        handleError(error);
+    }
+}
