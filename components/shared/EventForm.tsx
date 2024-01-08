09e6a6ad32a0644522a55cb9963a6d74951dd52f
Muhammad Fariz Maulana <19104035@ittelkom-pwt.ac.id>
2024-01-08 22:57:18 +0700

    implement uploadthing

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -5,17 +5,15 @@ import { useForm } from "react-hook-form"
 import * as z from 'zod'
 import { Button } from "@/components/ui/button"
 import {
-    Form,
-    FormControl,
-    FormDescription,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage,
+    Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { eventFormSchema } from "@/lib/validator"
 import { eventDefaultValues } from "@/constants"
+import Dropdown from "./Dropdown"
+import { Textarea } from "@/components/ui/textarea"
+import { FileUploader } from "./FileUploader"
+import { useState } from "react"
 
 type EventFormProps = {
     userId: string,
@@ -25,6 +23,7 @@ type EventFormProps = {
 // sample validation 
 
 const EventForm = ({ userId, type }: EventFormProps) => {
+    const [files, setFiles] = useState<File[]>([]);
     const initialValues = eventDefaultValues;
     // 1. Define your form.
     const form = useForm<z.infer<typeof eventFormSchema>>({
@@ -51,6 +50,48 @@ const EventForm = ({ userId, type }: EventFormProps) => {
                             <FormItem className="w-full">
                                 <FormControl>
                                     <Input placeholder="Event title" {...field} className="input-field" />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name="categoryId"
+                        render={({ field }) => (
+                            <FormItem className="w-full">
+                                <FormControl>
+                                    <Dropdown onChangeHandler={field.onChange} value={field.value} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                </div>
+                <div className="flex flex-col gap-5 md:flex-row">
+                    <FormField
+                        control={form.control}
+                        name="description"
+                        render={({ field }) => (
+                            <FormItem className="w-full">
+                                <FormControl className="h-72">
+                                    <Textarea placeholder="Event Description" {...field} className="textarea rounded-2xl" />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name="imageUrl"
+                        render={({ field }) => (
+                            <FormItem className="w-full">
+                                <FormControl className="h-72">
+                                    <FileUploader
+                                        onFieldChange={field.onChange}
+                                        imageUrl={field.value}
+                                        setFiles={setFiles}
+                                    />
                                 </FormControl>
                                 <FormMessage />
                             </FormItem>
