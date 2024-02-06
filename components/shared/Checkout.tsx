85bb4d617e1de49b55cd0b341e2f774ed4eb271e
Rizuraa <mfmaulana08@gmail.com>
2024-02-06 22:34:17 +0700

    solving

--- components/shared/Checkout.tsx
+++ components/shared/Checkout.tsx
@@ -1,7 +1,8 @@
 import React, { useEffect } from "react";
-import { Button } from "../ui/button";
-import { IEvent } from "@/lib/database/models/event.model";
 import { loadStripe } from "@stripe/stripe-js";
+
+import { IEvent } from "@/lib/database/models/event.model";
+import { Button } from "../ui/button";
 import { checkoutOrder } from "@/lib/actions/order.action";
 
 loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
@@ -20,6 +21,7 @@ const Checkout = ({ event, userId }: { event: IEvent; userId: string }) => {
       );
     }
   }, []);
+
   const onCheckout = async () => {
     const order = {
       eventTitle: event.title,
@@ -28,25 +30,24 @@ const Checkout = ({ event, userId }: { event: IEvent; userId: string }) => {
       isFree: event.isFree,
       buyerId: userId,
     };
+
     await checkoutOrder(order);
   };
 
   return (
-    <>
-      <form
-        action={onCheckout}
-        method="post"
+    <form
+      action={onCheckout}
+      method="post"
+    >
+      <Button
+        type="submit"
+        role="link"
+        size="lg"
+        className="button sm:w-fit"
       >
-        <Button
-          type="submit"
-          role="link"
-          size="lg"
-          className="button sm:w-fit"
-        >
-          {event.isFree ? "Get Ticket" : "Buy Tiket"}
-        </Button>
-      </form>
-    </>
+        {event.isFree ? "Get Ticket" : "Buy Ticket"}
+      </Button>
+    </form>
   );
 };
 
